e4b710616eca598a7559183cece9d98d1ff2f6b5
Jasminesonia <101569738+Jasminesonia@users.noreply.github.com>
2025-07-29 11:14:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://front-end-pearl-three.vercel.app"],
+    allow_origins=["https://shopana-eight.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
